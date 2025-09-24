3e0b529ca65f044bb3847e0e9e29012d55ab4828
Borislav Raynov <braynov@efellows.bg>
2025-09-24 12:55:38 +0300

    Add iam_role module

---
+++ src/iam_roles.tf
@@ -0,0 +1,11 @@
+module "iam_roles" {
+  source = "../modules/iam_role"
+  for_each = var.iam_roles
+
+	settings        = each.value
+  global_settings = local.global_settings
+
+  # client_config = {
+  #   landingzone_key = var.landingzone.key
+  # }
+}
