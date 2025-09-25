53f50c5a6ab90bae63eb350edb80386d016916e5
Borislav Raynov <braynov@efellows.bg>
2025-09-25 14:29:58 +0300

    Add kms_key refernce

--- src/modules/cloudwatch_log_group/_locals.tf
+++ src/modules/cloudwatch_log_group/_locals.tf
@@ -1,4 +1,9 @@
 locals {
+  kms_key_id = try(
+    var.resources[var.settings.kms_key_ref].arn, 
+    try(var.settings.kms_key_id, null)
+    )
+
   tags = merge(
     var.global_settings.tags,
     try(var.settings.tags, {})
--- src/modules/cloudwatch_log_group/main.tf
+++ src/modules/cloudwatch_log_group/main.tf
@@ -5,6 +5,7 @@ resource "aws_cloudwatch_log_group" "main" {
   skip_destroy        = try(var.settings.skip_destroy , null)
   log_group_class     = try(var.settings.log_group_class , null)
   retention_in_days   = try(var.settings.retention_in_days, null)
-  kms_key_id          = try(var.settings.kms_key_id, null)
+  # kms_key_id          = try(var.settings.kms_key_id, null)
+  kms_key_id          = local.kms_key_id
   tags                = local.tags
 }
