86f269ff9b8bd654d0d501c7baeb16f4f3f3d0a9
Borislav Raynov <braynov@efellows.bg>
2025-09-26 09:08:50 +0300

    Refactor modules

--- src/networking.tf
+++ src/networking.tf
@@ -97,3 +97,17 @@ module "internet_gateways" {
   #   landingzone_key = var.landingzone.key
   # }
 }
+
+module "security_groups" {
+  source = "./modules/_networking/security_group"
+  for_each = var.security_groups
+
+  settings        = each.value
+  global_settings = local.global_settings
+
+  resources = {}
+  
+  # client_config = {
+  #   landingzone_key = var.landingzone.key
+  # }
+}
