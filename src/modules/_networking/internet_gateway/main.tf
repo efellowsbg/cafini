44912cd70a0cec9b71d45ba904b03df5e779afb4
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-02 14:06:16 +0300

    change structure to include resource calls

--- src/modules/_networking/internet_gateway/main.tf
+++ src/modules/_networking/internet_gateway/main.tf
@@ -1,6 +1,10 @@
 resource "aws_internet_gateway" "main" {
-  # TODO: Doublecheck for missing arguments, reference to resources and dynamic blocks
-  vpc_id = try(var.settings.vpc_id, null)
+  vpc_id = try(
+    var.settings.vpc_id,
+    try(var.resources.vpc[each.key].id, null),
+    null
+  )
+
   region = try(var.settings.region, null)
   tags   = local.tags
 }
