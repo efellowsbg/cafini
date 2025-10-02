dc737bb4d9fbd787bce83e6282bc84f2320b6289
Borislav Raynov <braynov@efellows.bg>
2025-10-02 11:18:39 +0300

    Test with vpc module

--- src/launch_templates.tf
+++ src/launch_templates.tf
@@ -6,16 +6,16 @@ module "launch_templates" {
   global_settings = local.global_settings
 
   resources = {
-    iam_instance_profiles     = module.iam_instance_profiles
-    ec2_capacity_reservations = module.ec2_capacity_reservations
-    ec2_hosts                 = module.ec2_hosts
-    resourcegroups_groups     = module.resourcegroups_groups
-    placement_groups          = module.placement_groups
-    availability_zones        = module.availability_zones
-    kms_keys                  = module.kms_keys
-    security_groups           = module.security_groups
-    network_interfaces        = module.network_interfaces
-    vpcs                      = module.vpcs
+    # iam_instance_profiles     = module.iam_instance_profiles
+    # ec2_capacity_reservations = module.ec2_capacity_reservations
+    # ec2_hosts                 = module.ec2_hosts
+    # resourcegroups_groups     = module.resourcegroups_groups
+    # placement_groups          = module.placement_groups
+    # availability_zones        = module.availability_zones
+    # kms_keys                  = module.kms_keys
+    # security_groups           = module.security_groups
+    # network_interfaces        = module.network_interfaces
+    vpcs = module.vpcs
   }
 
   # client_config = {
