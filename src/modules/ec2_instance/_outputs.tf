e952fc8c9c9b5adf620b0505dcd59fa4007e6bd4
Borislav Raynov <braynov@efellows.bg>
2025-10-06 12:20:36 +0300

    Export module's arguments

--- src/modules/ec2_instance/_outputs.tf
+++ src/modules/ec2_instance/_outputs.tf
@@ -5,3 +5,11 @@ output "id" {
 output "arn" {
   value = aws_instance.main.arn
 }
+
+output "primary_network_interface_id" {
+  value = aws_instance.main.primary_network_interface_id
+}
+
+output "public_ip" {
+  value = aws_instance.main.public_ip
+}
