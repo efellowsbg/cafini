1aa0976c69340d357398a8472f0a7636f440c1e0
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-06 11:42:47 +0300

    change dns_options dynamic block to return empty object instead of list

--- src/modules/_networking/vpc_endpoint/main.tf
+++ src/modules/_networking/vpc_endpoint/main.tf
@@ -16,7 +16,7 @@ resource "aws_vpc_endpoint" "main" {
   tags                       = local.tags
 
   dynamic "dns_options" {
-    for_each = try(var.settings.dns_options, null) == null ? [] : [var.settings.dns_options]
+    for_each = try(var.settings.dns_options, {}) != {} ? [var.settings.dns_options] : []
     content {
       dns_record_ip_type                             = try(dns_options.value.dns_record_ip_type, null)
       private_dns_only_for_inbound_resolver_endpoint = try(dns_options.value.private_dns_only_for_inbound_resolver_endpoint, null)
