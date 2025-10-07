51e2aa2df8c8dafd2d362ab5078d819cf6ff8d98
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-07 15:01:54 +0300

    name is required

--- src/modules/iam_policy_attachment/main.tf
+++ src/modules/iam_policy_attachment/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy_attachment" "main" {
-  name       = try(var.settings.name, null)
+  name       = var.settings.name
   users      = try(local.users, null)
   roles      = try(local.roles, null)
   groups     = try(local.groups, null)
