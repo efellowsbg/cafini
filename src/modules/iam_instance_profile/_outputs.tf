7173cc2278d435c903aa0e89bf09c33aa6f2dbfa
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-07 10:02:06 +0300

    fix name and add unique_id output

--- src/modules/iam_instance_profile/_outputs.tf
+++ src/modules/iam_instance_profile/_outputs.tf
@@ -5,3 +5,7 @@ output "id" {
 output "arn" {
   value = aws_iam_instance_profile.main.arn
 }
+
+output "unique_id" {
+  value = aws_iam_instance_profile.main.unique_id
+}
--- src/modules/iam_instance_profile/main.tf
+++ src/modules/iam_instance_profile/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_instance_profile" "main" {
-  name        = var.settings.name
+  name        = try(var.settings.name, null)
   name_prefix = try(var.settings.name_prefix, null)
   path        = try(var.settings.path, null)
   role        = try(local.role, null)
