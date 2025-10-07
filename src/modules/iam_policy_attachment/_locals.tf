bff6de31abc92122e76348ae33a4be602bbf91a1
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-07 15:21:55 +0300

    refactor locals

--- src/modules/iam_policy_attachment/_locals.tf
+++ src/modules/iam_policy_attachment/_locals.tf
@@ -3,24 +3,26 @@ locals {
     var.resources.iam_policies[var.settings.policy_ref].arn,
     var.settings.policy_arn
   )
-  users = try(
-    [for user_ref in try(var.settings.user_refs, []) :
-      var.resources.iam_users[user_ref].name
-    ],
-    try(var.settings.users, [])
-  )
-  roles = try(
-    [for role_ref in try(var.settings.role_refs, []) :
-      var.resources.iam_roles[role_ref].name
-    ],
-    try(var.settings.roles, [])
-  )
-  groups = try(
-    [for group_ref in try(var.settings.group_refs, []) :
-      var.resources.iam_groups[group_ref].name
-    ],
-    try(var.settings.groups, [])
-  )
+  users = length(try(var.settings.user_refs, [])) > 0 ? [
+    for user_ref in try(var.settings.user_refs, []) : try(
+      var.resources.iam_users[user_ref].name,
+      null
+    )
+  ] : try(var.settings.users, null)
+
+  roles = length(try(var.settings.role_refs, [])) > 0 ? [
+    for role_ref in try(var.settings.role_refs, []) : try(
+      var.resources.iam_roles[role_ref].name,
+      null
+    )
+  ] : try(var.settings.roles, null)
+
+  groups = length(try(var.settings.group_refs, [])) > 0 ? [
+    for group_ref in try(var.settings.group_refs, []) : try(
+      var.resources.iam_groups[group_ref].name,
+      null
+    )
+  ] : try(var.settings.groups, null)
   tags = merge(
     var.global_settings.tags,
     try(var.settings.tags, {})
