12b2c50ca5d1349e90c6fbdb00e94998b82677d1
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-07 09:20:08 +0300

    CAF-14, make name mandatory

--- src/modules/iam_instance_profile/main.tf
+++ src/modules/iam_instance_profile/main.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_instance_profile" "main" {
-  # TODO: Add doublecheck for missing arguments, reference to resources and dynamic blocks
-  name        = try(var.settings.name, null)
+  name        = var.settings.name
   name_prefix = try(var.settings.name_prefix, null)
   path        = try(var.settings.path, null)
   role        = try(var.settings.role, null)
