71bcfe9c517b44ab2253695ce79425063163d3fd
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-07 09:33:06 +0300

    add role reference

--- src/modules/iam_instance_profile/main.tf
+++ src/modules/iam_instance_profile/main.tf
@@ -2,6 +2,6 @@ resource "aws_iam_instance_profile" "main" {
   name        = var.settings.name
   name_prefix = try(var.settings.name_prefix, null)
   path        = try(var.settings.path, null)
-  role        = try(var.settings.role, null)
+  role        = try(local.role, null)
   tags        = local.tags
 }
