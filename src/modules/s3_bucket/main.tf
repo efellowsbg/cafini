b71a7273578bf1df1b2d36bf8de970646e98bb64
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-08 15:48:25 +0300

    fix value

--- src/modules/s3_bucket/main.tf
+++ src/modules/s3_bucket/main.tf
@@ -9,7 +9,7 @@ resource "aws_s3_bucket" "main" {
     content {
       rule {
         apply_server_side_encryption_by_default {
-          sse_algorithm     = try(server_side_encryption_configuration.value.sse_algorithm, "AES256")
+          sse_algorithm     = try(server_side_encryption_configuration.value.sse_algorithm, null)
           kms_master_key_id = try(local.kms_key_arn, null)
         }
         bucket_key_enabled = try(server_side_encryption_configuration.value.bucket_key_enabled, false)
