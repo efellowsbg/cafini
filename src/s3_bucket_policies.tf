38fc0e645a6ef3c1397dbaca20d0a736fc32d3d0
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-14 11:10:22 +0300

    fix resource for iam_policies

--- src/s3_bucket_policies.tf
+++ src/s3_bucket_policies.tf
@@ -5,7 +5,7 @@ module "s3_bucket_policies" {
   settings        = each.value
   global_settings = local.global_settings
   resources = {
-    s3_buckets           = module.s3_buckets
-    iam_policy_documents = module.iam_policy_documents
+    s3_buckets   = module.s3_buckets
+    iam_policies = module.iam_policy
   }
 }
