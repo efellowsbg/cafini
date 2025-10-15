b1c3619f6ef0426f18fbf466e856d28a56e956ab
martinvladimirov220116 <martin.vladimirov@efellows.bg>
2025-10-15 17:31:59 +0300

    remove . resources from resources

--- src/eks_node_groups.tf
+++ src/eks_node_groups.tf
@@ -3,15 +3,16 @@ module "eks_node_groups" {
 
   for_each = var.eks_node_groups
 
-  settings = each.value
+  settings        = each.value
+  global_settings = var.global_settings
 
   resources = {
-    eks_clusters     = module.eks_clusters.resources
-    iam_roles        = module.iam_roles.resources
-    vpcs             = module.vpcs.resources
-    launch_templates = module.launch_templates.resources
-    security_groups  = module.security_groups.resources
+    eks_clusters     = module.eks_clusters
+    iam_roles        = module.iam_roles
+    vpcs             = module.vpcs
+    launch_templates = module.launch_templates
+    security_groups  = module.security_groups
   }
 
-  global_settings = var.global_settings
+
 }
