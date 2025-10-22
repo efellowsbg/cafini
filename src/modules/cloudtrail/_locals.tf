b38426ba7a4322e2e6f361bc7259daa25fe5c876
Borislav Raynov <braynov@efellows.bg>
2025-10-22 11:44:55 +0300

    Fix linting

--- src/modules/cloudtrail/_locals.tf
+++ src/modules/cloudtrail/_locals.tf
@@ -4,14 +4,14 @@ locals {
     var.settings.s3_bucket_name
   )
 
-  cloud_watch_logs_group_arn = try(
-    var.resources.cloudwatch_log_groups[var.settings.cloudwatch_logs_group_ref].arn,
-    try(var.settings.cloud_watch_logs_group_arn, null)
-  )
-
   cloud_watch_logs_role_arn = try(
     var.resources.iam_roles[var.settings.cloud_watch_logs_role_arn_ref].arn,
     try(var.settings.cloud_watch_logs_role_arn, null)
+  )
+
+  cloud_watch_logs_group_arn = try(
+    var.resources.cloudwatch_log_groups[var.settings.cloudwatch_logs_group_ref].arn,
+    try(var.settings.cloud_watch_logs_group_arn, null)
   )
 
   kms_key_id = try(
