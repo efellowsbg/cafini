b20a808ce6bc7cfd0947975f31e121cabedc8abc
Borislav Raynov <braynov@efellows.bg>
2025-10-31 11:14:00 +0200

    Comment out not created module's resources

--- src/db_instances.tf
+++ src/db_instances.tf
@@ -11,9 +11,9 @@ module "db_instances" {
     security_groups               = module.security_groups
     db_instances                  = modules.db_instances
     directory_service_directories = module.directory_service_directories
-    db_option_groups              = module.db_option_groups
-    db_parameter_groups           = module.db_parameter_groups
-    db_subnet_groups              = module.db_subnet_groups
+    # db_option_groups              = module.db_option_groups
+    # db_parameter_groups           = module.db_parameter_groups
+    # db_subnet_groups              = module.db_subnet_groups
   }
 
   # client_config = {
